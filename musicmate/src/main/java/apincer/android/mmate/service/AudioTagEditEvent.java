8a53844aa0bed99d22761e439e6898e60659accd
thawee <thaweemail@gmail.com>
2022-01-19 10:31:07 +0700

    update xchange item list between mainand tag activity

---
+++ musicmate/src/main/java/apincer/android/mmate/service/AudioTagEditEvent.java
@@ -0,0 +1,27 @@
+package apincer.android.mmate.service;
+
+import java.util.Collection;
+import java.util.List;
+
+import apincer.android.mmate.objectbox.AudioTag;
+import apincer.android.mmate.repository.SearchCriteria;
+
+public class MessageEvent {
+    public final String message;
+    public final SearchCriteria criteria;
+    public final List<AudioTag> items;
+
+    public MessageEvent(String message, SearchCriteria criteria, List<AudioTag> items) {
+        this.message = message;
+        this.criteria = criteria;
+        this.items = items;
+    }
+
+    public SearchCriteria getSearchCriteria() {
+        return criteria;
+    }
+
+    public Collection<? extends AudioTag> getItems() {
+        return items;
+    }
+}
